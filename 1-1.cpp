4bb80d023e0203b25e73f3a1e1ed25516edd6f2e
boyali <boya.li@jjay.cuny.edu>
2018-03-01 15:24:47 -0500

    homework

--- 1-1.cpp
+++ 1-1.cpp
@@ -1,4 +1,21 @@
-//
-// Created by Boya Li on 2/8/18.
-//
+#include <iostream>
 
+int main()
+{
+    int intergerType;
+    short shortType;
+    float floatType;
+    double doubleType;
+    long longType;
+    long long longlongType;
+    unsigned long ulongType;
+
+    printf("Size of int: %ld bytes \n", sizeof(intergerType));
+    printf("Size of short: %ld bytes \n", sizeof(shortType));
+    printf("Size of float: %ld bytes \n", sizeof(floatType));
+    printf("Size of double: %ld bytes \n", sizeof(doubleType));
+    printf("Size of long: %ld bytes \n", sizeof(longType));
+    printf("Size of long long: %ld bytes \n", sizeof(longlongType));
+    printf("Size of unsigned long: %ld bytes \n", sizeof(ulongType));
+    return 0;
+}
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,21 @@
-#include<iostrream>
-using namespace std;
-main()
+#include <iostream>
+
+int main()
 {
-    st
+    int intergerType;
+    short shortType;
+    float floatType;
+    double doubleType;
+    long longType;
+    long long longlongType;
+    unsigned long ulongType;
+
+    printf("Size of int: %ld bytes \n", sizeof(intergerType));
+    printf("Size of short: %ld bytes \n", sizeof(shortType));
+    printf("Size of float: %ld bytes \n", sizeof(floatType));
+    printf("Size of double: %ld bytes \n", sizeof(doubleType));
+    printf("Size of long: %ld bytes \n", sizeof(longType));
+    printf("Size of long long: %ld bytes \n", sizeof(longlongType));
+    printf("Size of unsigned long: %ld bytes \n", sizeof(ulongType));
+    return 0;
 }
