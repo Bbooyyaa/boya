6692871a8a5ff304d37a60fa423586eeec3a38d4
boyali <boya.li@jjay.cuny.edu>
2018-03-01 15:39:45 -0500

    homework

--- 2-1.cpp
+++ 2-1.cpp
@@ -1,4 +1,21 @@
-//
-// Created by Boya Li on 2/27/18.
-//
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int m;
+    cout<<"Enter the student grade: ";
+    cin>>m;
+    if (m>=90)
+        cout<<"A\n";
+    else if (m>=80)
+        cout<<"B\n";
+    else if (m>=70)
+        cout<<"C\n";
+    else if (m>=60)
+        cout<<"D\n";
+    else
+        cout<<"F\n";
+    return 0;
+}
